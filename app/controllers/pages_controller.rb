e032b01819b5c23e68b87a01e9e6afcfa0895ab4
Josiah Schaefer <penname.silas@gmail.com>
2015-10-17 22:58:06 +0000

    added redirect to dashboard for logged in users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+  before_action :to_dashboard, only: [:welcome]
+  before_action :require_user, only: [:dashboard]
+  
   def welcome
     
   end
@@ -6,5 +9,13 @@ class PagesController < ApplicationController
   def dashboard
     @user = current_user
     @homeschool = @user.homeschool
+  end
+  
+  private
+  
+  def to_dashboard
+    if logged_in?
+      redirect_to dashboard_path
+    end
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,4 +7,11 @@ DatabaseCleaner.strategy = :truncation
 DatabaseCleaner.clean
 
 describe PagesController do
+  describe "GET #welcome"
+  it "redirects to dashboard if already logged in" do
+    homeschool = Fabricate(:homeschool)
+    session[:user_id] = Fabricate(:user, homeschool_id: homeschool.id).id
+    get :welcome
+    expect(response).to redirect_to dashboard_path
+  end
 end
